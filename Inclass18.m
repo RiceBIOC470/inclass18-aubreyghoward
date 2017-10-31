251b04b80b516d3fb7b50a0731bd88e9101bc95f
aubreyghoward <aubrey.g.howard@rice.edu>
2017-10-31 15:50:11 -0500

    Partial Q1

--- Inclass18.m
+++ Inclass18.m
@@ -5,6 +5,39 @@
 % Then, try to find as many of the cells as you can using the
 % imfindcircles routine with appropriate parameters. 
 % Display the image with the circles drawn on it.  
+reader = bfGetReader('yeast.tif');
+
+fid = fopen('readerData.txt','w');
+for i = 1:length(reader)
+    fprintf(fid,'Data for image number: %d \n',i);
+    fprintf(fid,'Channel Num: %d \n',(reader.getSizeC));
+    fprintf(fid,'Time Scale: %d \n',(reader.getSizeT));
+    fprintf(fid,'Z slices: %d \n \n',(reader.getSizeZ));
+end
+fclose(fid);
+
+chan = 1;
+time = 1;
+zplane = 1;
+iplane = reader.getIndex(zplane-1,chan-1,time-1)+1;
+img1 = bfGetPlane(reader,iplane);
+
+img1 = im2double(img1);
+img1 = imfilter(img1,fspecial('gaussian',4,2));
+
+Iy = imfilter(img1,fspecial('sobel'),'replicate');
+Ix = imfilter(img1,fspecial('sobel'),'replicate');
+edge_img1 = sqrt(Ix.^2+Iy.^2);
+
+[centers,radius] = imfindcircles(edge_img1,'Sensitivity',0.95);
+figure;imshow(edgeimg,[]);hold on;
+for ii = length(centers);
+    drawcircle(centers(ii,:),radius(ii),'m');
+end
+hold off;
+
+imshow(img1,[]);
+
 
 % Problem 2. (A) Draw two sets of 10 random numbers - one from the integers
 % between 1 and 9 and the second from the integers between 1 and 10. Run a
